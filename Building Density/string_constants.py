5008b3e65d7bb5fb17be9d736d82560c80a2ea62
ShakyaRupasinghe4168 <139677601+ShakyaRupasinghe4168@users.noreply.github.com>
2025-07-31 12:45:41 +0530

    Update string_constants.py

--- Building Density/string_constants.py
+++ Building Density/string_constants.py
@@ -1,11 +1,6 @@
-# File: string_constants.py
 
-# Column names in MDT data
 LAT_COL = "Latitude_of_MDT"
 LON_COL = "Longitude_of_MDT"
 
-# Geometry column name
 GEOMETRY = "geometry"
-
-# Output column for building density
 BUILDING_DENSITY = "Building_Density"
