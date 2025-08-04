c8386f9b7399508cc87b571f4f693ebefbcdae49
ShakyaRupasinghe4168 <139677601+ShakyaRupasinghe4168@users.noreply.github.com>
2025-08-04 11:56:34 +0530

    Road density

---
+++ Road Density/file_paths.py
@@ -0,0 +1,10 @@
+# File: file_paths.py
+
+# Path to the MDT CSV file
+MDT_DATA_FILE = "Sri-Lanka.csv"
+
+# Path to the building shapefile (can also be GeoJSON)
+ROAD_DATA_FILE = "gis_osm_roads_free_1.shp"
+
+# Output path
+OUTPUT_FILE = "mdt_with_road_density.csv"
